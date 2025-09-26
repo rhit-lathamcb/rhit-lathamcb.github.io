ed199ee4d16bc79a9fc9d891261362c29a948cf2
Carter Latham <lathamcb@rose-hulman.edu>
2025-09-26 17:50:36 -0400

    PLEASE

--- scripts.js
+++ scripts.js
@@ -5,8 +5,8 @@ console.log("do it run")
 headerTemplate.innerHTML =      
   '<header><nav><a href="index.html">Main Page</a><p> | </p><a href="portfolio.html">Portfolio</a><p> | </p><a href="resume.html">Resume</a></nav></header>'
 
-let bodyTag = document.getElementByTagName("body");
-let mainTag = document.getElementByTagName("main");
+let bodyTag = document.getElementsByTagName("body")[0];
+let mainTag = document.getElementsByTagName("main")[0];
 // maybe these need to be node objects but aren't idk
 headerTemplate.insertBefore(bodyTag, mainTag);
 // document.body.appendChild(headerTemplate.content);
