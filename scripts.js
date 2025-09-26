be55ec5ea824cd28e30696839eed78cc3d606482
Carter Latham <lathamcb@rose-hulman.edu>
2025-09-26 18:02:45 -0400

    teset

--- scripts.js
+++ scripts.js
@@ -1,12 +1,10 @@
 const headerTemplate = document.createElement('template');
 
-console.log("do it run")
-
 headerTemplate.innerHTML =      
   '<header><nav><a href="index.html">Main Page</a><p> | </p><a href="portfolio.html">Portfolio</a><p> | </p><a href="resume.html">Resume</a></nav></header>'
 
 let bodyTag = document.getElementsByTagName("body")[0];
 let mainTag = document.getElementsByTagName("main")[0];
 // maybe these need to be node objects but aren't idk
-headerTemplate.content.insertBefore(bodyTag, mainTag);
+bodyTag.insertBefore(headerTemplate, mainTag);
 // document.body.appendChild(headerTemplate.content);
