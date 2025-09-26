53f19286c28a527dc116ff82975bbded6c01fee7
Carter Latham <lathamcb@rose-hulman.edu>
2025-09-26 18:15:20 -0400

    cleaning up a bit

--- scripts.js
+++ scripts.js
@@ -1,17 +1,14 @@
+//www.freecodecamp.org/news/reusable-html-components-how-to-reuse-a-header-and-footer-on-a-website/
 const headerTemplate = document.createElement('template');
-
 headerTemplate.innerHTML =      
   '<header><nav><a href="index.html">Main Page</a> | <a href="portfolio.html">Portfolio</a> | <a href="resume.html">Resume</a></nav></header>'
-
 let bodyTag = document.getElementsByTagName("body")[0];
 let mainTag = document.getElementsByTagName("main")[0];
 bodyTag.insertBefore(headerTemplate.content, mainTag);
 
 const footerTemplate = document.createElement('template');
-
 footerTemplate.innerHTML =      
-  '<footer><a href="https://www.linkedin.com/in/carter-latham-862112329/">LinkedIn</a><a href="https://app.joinhandshake.com/profiles/fx7nxk">Handshake</a><a href="https://github.com/rhit-lathamcb">GitHub</a></footer>'
-
+  '<footer><a href="https://www.linkedin.com/in/carter-latham-862112329/">LinkedIn</a> | <a href="https://app.joinhandshake.com/profiles/fx7nxk">Handshake</a> | <a href="https://github.com/rhit-lathamcb">GitHub</a></footer>'
 bodyTag.appendChild(footerTemplate.content);
 
 
