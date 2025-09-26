de8f7a05fd876de47b39566336b913ccd2d2004e
Carter Latham <lathamcb@rose-hulman.edu>
2025-09-26 13:59:17 -0400

    test header fix

--- scripts.js
+++ scripts.js
@@ -3,12 +3,6 @@ const headerTemplate = document.createElement('template');
 console.log("do it run")
 
 headerTemplate.innerHTML =      
-  <header>
-    <nav>
-      <a href="index.html">index.html</a>
-      <a href="portfolio.html">portfolio.html</a>
-      <a href="resume.html">resume.html</a>
-    </nav>
-  </header>
+  '<header><nav><a href="index.html">index.html</a><a href="portfolio.html">portfolio.html</a><a href="resume.html">resume.html</a></nav></header>'
 
 document.body.appendChild(headerTemplate.content);
