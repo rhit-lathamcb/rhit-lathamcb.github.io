7ad1829dd4c3defe2c4a0544a2a70032c34feba5
Carter Latham <lathamcb@rose-hulman.edu>
2025-09-26 23:06:15 -0400

    added "interesting" js

--- scripts.js
+++ scripts.js
@@ -1,25 +1,20 @@
 //www.freecodecamp.org/news/reusable-html-components-how-to-reuse-a-header-and-footer-on-a-website/
 const headerTemplate = document.createElement('template');
 headerTemplate.innerHTML =      
-  '<header><nav><a href="index.html">Main Page</a> | <a href="portfolio.html">Portfolio</a> | <a href="resume.html">Resume</a></nav></header>'
+  '<header><nav><a href="index.html" id="main-page-link">Main Page</a> | <a href="portfolio.html" id="portfolio-link">Portfolio</a> | <a href="resume.html" id="resume-link">Resume</a></nav></header>'
 let bodyTag = document.getElementsByTagName("body")[0];
 let mainTag = document.getElementsByTagName("main")[0];
 bodyTag.insertBefore(headerTemplate.content, mainTag);
 
 const footerTemplate = document.createElement('template');
 footerTemplate.innerHTML =      
-  '<footer><a href="https://www.linkedin.com/in/carter-latham-862112329/">LinkedIn</a> | <a href="https://app.joinhandshake.com/profiles/fx7nxk">Handshake</a> | <a href="https://github.com/rhit-lathamcb">GitHub</a></footer>'
+  '<footer><a href="https://www.linkedin.com/in/carter-latham-862112329/" id="linkedin-link">LinkedIn</a> | <a href="https://app.joinhandshake.com/profiles/fx7nxk" id="handshake-link">Handshake</a> | <a href="https://github.com/rhit-lathamcb" id="github-link">GitHub</a></footer>'
 bodyTag.appendChild(footerTemplate.content);
 
-/* https://www.w3schools.com/jsref/met_win_settimeout.asp - need timer for popup */
-const myTimeout = setTimeout(popupActivate, 6000);
-
-function popupActivate() {
-  let popupElement = document.getElementById("my-popup");
-  popupElement.classList.remove("invisible");
-  popupElement.addEventListener("onclick", (e) => {
-    popupElement.classList.add("invisible")
-  })
+/* https://www.w3schools.com/js/js_timing.asp - I decided it would be a good idea to make sure the user has seen everything they want to see. I suppose some users could miss the links, especially the ones in the footer */
+function popupAfterMinute() {
+  setTimeout(0, 6000);
+  window.alert("Be sure to check out all of the links!")
 }
 
 
